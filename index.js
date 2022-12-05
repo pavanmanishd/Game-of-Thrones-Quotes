1cef5ebe43a944360d395c57635280390e000b7e
Pavan Manish <108605548+pavanmanishd@users.noreply.github.com>
2022-12-05 15:55:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const button = document.getElementById("btn");
 
-
 function fetchdata(){
 fetch("https://api.gameofthronesquotes.xyz/v1/random",{
            cache: "no-store",
@@ -20,24 +19,3 @@ const load = true;
 load ? fetchdata() : null;
 
 button.addEventListener("click",fetchdata);
-
-
-
-
-// let p = document.querySelector("#p");
-// const button = document.getElementById("btn");
-
-// function fetchdata() {
-//     fetch("https://api.adviceslip.com/advice?", {
-//       cache: "no-store",
-//     })
-//       .then((response) => response.json())
-
-      
-//       .then((slipObj) => (textArea.innerText = slipObj.slip.advice));
-//   }
-
-// const load = true; 
-// load ? fetchdata() : null // first time load
-
-// button.addEventListener("click", fetchdata);
