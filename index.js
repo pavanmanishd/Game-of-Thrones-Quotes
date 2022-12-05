4e9ba47674a6f9d51b9ef5047485c6a6e9936a64
Pavan Manish <108605548+pavanmanishd@users.noreply.github.com>
2022-12-05 23:25:38 +0530

    New Feature

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 const button = document.getElementById("btn");
 const url = "https://api.gameofthronesquotes.xyz/v1/random";
+var k = url;
 
-function fetchdata(){
-fetch(x,{
-           cache: "no-store",
-         })
-.then(response => {
-    return response.json();
-})
-.then(data => {
-    
-    document.getElementById("main-quote").innerHTML = data.sentence ;
-    document.getElementById("character").innerHTML = "- "+data.character.name.toUpperCase();
+function fetchdata() {
+    fetch(k, {
+        cache: "no-store",
+    })
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
 
-});
+            document.getElementById("main-quote").innerHTML = data.sentence;
+            document.getElementById("character").innerHTML = "- " + data.character.name.toUpperCase();
+
+        });
 }
 
-const load = true; 
+const load = true;
 load ? fetchdata() : null;
 
 function getVal() {
-    const val = document.querySelector('input').value;
-     return url.concat(val);
+    const val = document.querySelector('input').value.toLowerCase();
+    if (val === "") {
+        return url;
+    }
+    else {
+        return url.substring(0, url.length - 6).concat("author/").concat(val);
+    }
 }
 
-button.addEventListener("click",function(){
-    const x = getVal();
+button.addEventListener("click", function () {
+    k = getVal();
     fetchdata();
 });
 
